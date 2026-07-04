921cde2b5b0aa78dd45a09901e8ad177d379a688
lonezebra <lonezebra@protonmail.com>
2026-07-03 21:54:17 -0700

    cocoa

--- src/finance_ai/core/qt.py
+++ src/finance_ai/core/qt.py
@@ -1,9 +1,12 @@
 import os
+from pathlib import Path
+
 from PySide6.QtCore import QLibraryInfo
 
 
 def configure_qt() -> None:
-    plugin_path = QLibraryInfo.path(QLibraryInfo.PluginsPath)
+    plugin_root = Path(QLibraryInfo.path(QLibraryInfo.PluginsPath))
+    platforms_path = plugin_root / "platforms"
 
-    if plugin_path:
-        os.environ.setdefault("QT_QPA_PLATFORM_PLUGIN_PATH", plugin_path)
+    if platforms_path.exists():
+        os.environ.setdefault("QT_QPA_PLATFORM_PLUGIN_PATH", str(platforms_path))
